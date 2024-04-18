ff972a21c8198a3f4e24aa07d4a12d71c977777e
Khomutov Ivan <khomutovivan2409@mail.ru>
2024-04-18 16:43:49 +0700

    менюшка

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from Classes import staff,tables,menu,boss
 import asyncio
+import os
+menu = menu.Menu
+clear = lambda: os.system('cls')
+clear()
 Boss=boss.owner()
 Vasya = staff.Staff()
 t1 = tables.Table()
-asyncio.run(Vasya.cook(t1,Boss))
 Boss.stat()
+while True:
+    print(f'\n1: Новый заказ\n'
+          f'2: Повысить уровень игрока\n'
+          f'3: Повысиль лвл персонала\n'
+          f'4: купить персонал\n '
+          f'5: купить столик\n'
+          f'6: Статистика')
+    a = int(input())
+    if a == 1:
+        clear()
+        asyncio.run(Vasya.cook(t1, Boss))
+
+    if a == 2:
+        clear()
+        print(Boss.bosslvlup())
+    if a == 3:
+        clear()
+        print(Boss.personlvlup())
+    if a == 6:
+        clear()
+        print(Boss.stat())
+
