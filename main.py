33e5bcbd0daa071ecf943c66b05fb4fd73c0670f
Khomutov Ivan <khomutovivan2409@mail.ru>
2024-04-18 16:53:50 +0700

    ////

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ from Classes import staff,tables,menu,boss
 import asyncio
 import os
 menu = menu.Menu
-clear = lambda: os.system('cls')
-clear()
+
 Boss=boss.owner()
-Vasya = staff.Staff()
-t1 = tables.Table()
+Staff_list = [staff.Staff()]
+Tables_list = [tables.Table()]
+
 Boss.stat()
 while True:
-    print(f'\n1: Новый заказ\n'
+    freeTable = []
+    freeStaff = []
+    print(f'\n1: Взять заказ\n'
           f'2: Повысить уровень игрока\n'
           f'3: Повысиль лвл персонала\n'
           f'4: купить персонал\n '
           f'5: купить столик\n'
           f'6: Статистика')
+    for i in Tables_list:
+        if i.free:
+            freeTable.append(i)
+            i.take_order(menu,Boss)
+
     a = int(input())
     if a == 1:
-        clear()
-        asyncio.run(Vasya.cook(t1, Boss))
 
     if a == 2:
-        clear()
         print(Boss.bosslvlup())
     if a == 3:
-        clear()
         print(Boss.personlvlup())
     if a == 6:
-        clear()
         print(Boss.stat())
 
