ac83fe434fb2dd25efc228965f37b86f2ce93388
Djerdux <92996251+Djerdux@users.noreply.github.com>
2024-04-18 16:14:21 +0700

    update codes

--- Classes/boss.py
+++ Classes/boss.py
@@ -18,11 +18,13 @@ class owner:
         elif self.xp<10*person.level:
             print(f'Вам не хватает опыта и денег для повышения уровня у {person.name}. Вам нужно: {(10 * person.level) - self.xp} XP')
     def bosslvlup(self):
-        if self.money>=(20*self.xp) and self.xp>=(20*self.xp):
-            print(f'Вы повысили уровень с {self.xp} до {self.xp+1}\nС вашего баланса списано: {self.xp>=(20*self.xp)}\nОстаток: {self.money}')
-        elif self.money<(self.xp>=(20*self.xp)) and self.xp<(20*self.xp):
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.xp)-self.money}$\n{(20*self.xp)-self.xp} XP')
-        elif self.money<20*self.xp:
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно ещё: {(20*self.xp)-self.money}$')
-        elif self.xp<20*self.xp:
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.xp) - self.xp} XP')
+        if self.money>=(20*self.lvl) and self.xp>=(20*self.lvl):
+            print(f'Вы повысили уровень с {self.lvl} до {self.lvl+1}\nС вашего баланса списано: {20*self.lvl}\nОстаток: {self.money}')
+            self.lvl+=1
+            self.money-=20*self.xp
+        elif self.money<(self.xp>=(20*self.lvl)) and self.xp<(20*self.lvl):
+            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.lvl)-self.money}$\n{(20*self.lvl)-self.xp} XP')
+        elif self.money<20*self.lvl:
+            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно ещё: {(20*self.lvl)-self.money}$')
+        elif self.xp<20*self.lvl:
+            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.lvl) - self.xp} XP')
