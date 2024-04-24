55c8a8480de7ad942f86ceb1d097541d3118939d
Djerdux <92996251+Djerdux@users.noreply.github.com>
2024-04-24 18:00:56 +0700

    update codes

--- Classes/boss.py
+++ Classes/boss.py
@@ -1,11 +1,11 @@
-
+from Classes import staff,tables
 class owner:
     def __init__(self):
-        self.xp=0
-        self.money=0
+        self.xp=1000
+        self.money=1000
         self.lvl=1
-        self.personal=[]
-        self.table=[]
+        self.personal=[staff.Staff()]
+        self.tabless=[tables.Table()]
     def stat(self):
         print(f'Ваш опыт: {self.xp} XP.\nВаш баланс: {self.money}$\nВаш уровень: {self.lvl}')
     def personlvlup(self,person):
@@ -20,32 +20,41 @@ class owner:
         elif self.xp<10*person.level:
             print(f'Вам не хватает опыта и денег для повышения уровня у {person.name}. Вам нужно: {(10 * person.level) - self.xp} XP')
     def bosslvlup(self):
-        if self.money>=(20*self.lvl) and self.xp>=(20*self.lvl):
-            print(f'Вы повысили уровень с {self.lvl} до {self.lvl+1}\nС вашего баланса списано: {20*self.lvl}\nОстаток: {self.money}')
-            self.lvl+=1
-            self.money-=20*self.xp
-        elif self.money<(self.xp>=(20*self.lvl)) and self.xp<(20*self.lvl):
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.lvl)-self.money}$\n{(20*self.lvl)-self.xp} XP')
-        elif self.money<20*self.lvl:
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно ещё: {(20*self.lvl)-self.money}$')
-        elif self.xp<20*self.lvl:
-            print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.lvl) - self.xp} XP')
+        if self.lvl>1:
+            if self.money>=(20*self.lvl) and self.xp>=(20*self.lvl):
+                print(f'Вы повысили уровень с {self.lvl} до {self.lvl+1}\nС вашего баланса списано: {20*self.lvl}\nОстаток: {self.money-20*self.lvl}')
+                self.lvl+=1
+                self.money-=20*self.lvl
+            elif self.money<(self.xp>=(20*self.lvl)) and self.xp<(20*self.lvl):
+                print(f'Вам не хватает опыта и денег для повышения уровня. Вам нужно: {(20*self.lvl)-self.money}$\n{(20*self.lvl)-self.xp} XP')
+            elif self.money<20*self.lvl:
+                print(f'Вам не хватает денег для повышения уровня. Вам нужно ещё: {(20*self.lvl)-self.money}$')
+            elif self.xp<20*self.lvl:
+                print(f'Вам не хватает опыта для повышения уровня. Вам нужно: {(20*self.lvl) - self.xp} XP')
+        else:
+            if self.money>=(20*self.lvl):
+                print(f'Вы повысили уровень с {self.lvl} до {self.lvl+1}\nС вашего баланса списано: {20*self.lvl}\nОстаток: {self.money-20*self.lvl}')
+                self.lvl+=1
+                self.money-=20*self.lvl
+            elif self.money<20*self.lvl:
+                print(f'Вам не хватает денег для повышения уровня. Вам нужно ещё: {(20*self.lvl)-self.money}$')
+
     def buynewperson(self,person):
-        if self.money>=30 and self.lvl>=2:
-            self.personal.append(person)
-            self.money-=30
-            print(f'Вы успешно приобрели официанта.\nС вашего баланса списано 30$\nОстаток по балансу: {self.money-30}\nКоличество кадров: {len(self.personal)}')
-        elif self.money<30 and self.lvl<=2:
-            print(f'Вам не хватает уровня и денег для покупки персонала. Вам нужно: {30-self.money}$\n+1 LVL')
-        elif self.money<30:
-            print(f'Вам не хватает денег для покупки персонала. Вам нужно: {30 - self.money}$')
-        elif self.lvl<=30:
-            print(f'Вам не хватает уровня для покупки персонала. Вам нужен еще 1 LVL')
+            if self.money>=30 and self.lvl>=2:
+                self.personal.append(person)
+                self.money-=30
+                print(f'Вы успешно приобрели официанта.\nС вашего баланса списано 30$\nОстаток по балансу: {self.money-30}\nКоличество кадров: {len(self.personal)}')
+            elif self.money<30 and self.lvl<2:
+                print(f'Вам не хватает уровня и денег для покупки персонала. Вам нужно: {30-self.money}$ и +1 LVL')
+            elif self.money<30:
+                print(f'Вам не хватает денег для покупки персонала. Вам нужно: {30 - self.money}$')
+            elif self.lvl<=30:
+                print(f'Вам не хватает уровня для покупки персонала. Вам нужен еще 1 LVL')
     def buynewtable(self,table):
         if self.money>=30 and self.lvl>=2:
-            self.table.append(table)
+            self.tabless.append(table)
             self.money-=30
-            print(f'Вы успешно приобрели стол.\nС вашего баланса списано 30$\nОстаток по балансу: {self.money-30}\nКоличество столов: {len(self.table)}')
+            print(f'Вы успешно приобрели стол.\nС вашего баланса списано 30$\nОстаток по балансу: {self.money-30}\nКоличество столов: {len(self.tabless)}')
         elif self.money<30 and self.lvl<=2:
             print(f'Вам не хватает уровня и денег для покупки стола. Вам нужно: {30-self.money}$\n+1 LVL')
         elif self.money<30:
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 menu = menu.Menu()
 
 player=boss.owner()
-Staff_list = [staff.Staff()]
-Tables_list = [tables.Table()]
+Staff_list = player.personal
+Tables_list = player.tabless
 
 
 
@@ -17,9 +17,9 @@ while True:
     freeStaff = []
     print(f'\n1: Взять заказ\n'
           f'2: Повысить уровень игрока\n'
-          f'3: Повысиль лвл персонала\n'
-          f'4: купить персонал\n '
-          f'5: купить столик\n'
+          f'3: Повысить лвл персонала\n'
+          f'4: Купить персонал\n'
+          f'5: Купить столик\n'
           f'6: Статистика')
     for i in Tables_list:
         if i.free:
@@ -28,21 +28,25 @@ while True:
     for i in Staff_list:
         if i.busy==False:
            freeStaff.append(i.id)
-    a = int(input())
+    print("_"*100)
+    a = int(input('Введите нужную функцию: '))
+    print("_"*100)
     if a == 1:
         if len(freeStaff)>=1:
             print('Свободные рабочие:')
             for i in freeStaff:
-                print(f'ID-индетификатор сотрудника: {i}\nИмя: {Staff_list[i-1].name}\nУровень: {Staff_list[i-1].level}\n{"_"*50}')
-            inp=int(input('Введите ID-индетификатор нужного сотрудника'))
+                print(f'ID-индетификатор сотрудника: {i}\nИмя: {Staff_list[i-1].name}\nУровень: {Staff_list[i-1].level}\n{"_"*100}')
+            inp=int(input('Введите ID-индетификатор нужного сотрудника: '))
             while inp not in freeStaff:
-                    inp=int(input('Данный сотрудник занят/несуществует. Повторите ввод:'))
+                    inp=int(input('Данный сотрудник занят/несуществует. Повторите ввод: '))
             print('Выберите стол:')
             for i in busyTable:
                 print(f'Номер стола: {i}\nЗаказ: {Tables_list[i-1].order}\n{"_"*100}')
-            inp2=int(input('Введите номер нужного стола:'))
+            inp2=int(input('Введите номер нужного стола: '))
             while inp2 not in busyTable:
                 inp2=int(input('Данный стол свободен/несуществует. Повторите ввод:'))
+            Tables_list[inp2-1].free=True
+            busyTable.clear()
             asyncio.run(Staff_list[inp-1].cook(Tables_list[inp2-1],player))
 
 
@@ -50,7 +54,14 @@ while True:
     if a == 2:
         player.bosslvlup()
     if a == 3:
-        player.personlvlup()
+        for i in Staff_list:
+            print(f'ID-индетификатор сотрудника: {i}\nИмя: {i.name}\nУровень: {i.level}\n{"_"*100}')
+        inp=int(input('Выберите сотрудника которому повысить уровень: '))
+        player.personlvlup(Staff_list[inp-1])
+    if a==4:
+        player.buynewperson(staff.Staff())
+    if a==5:
+        player.buynewtable(tables.Table())
     if a == 6:
         player.stat()
 
