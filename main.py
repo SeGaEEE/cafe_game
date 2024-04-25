edfa41feeb6728520ad2e98956ee3a6550327dcd
Khomutov Ivan <khomutovivan2409@mail.ru>
2024-04-25 16:07:51 +0700

    фикс мейна

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ player=boss.owner()
 Staff_list = player.personal
 Tables_list = player.tabless
 
-
-
-
+busyTable = []
+freeStaff = []
 player.stat()
 while True:
-    busyTable = []
-    freeStaff = []
+
     print(f'\n1: Взять заказ\n'
           f'2: Повысить уровень игрока\n'
           f'3: Повысить лвл персонала\n'
@@ -26,27 +24,27 @@ while True:
             busyTable.append(i.id)
             i.take_order(menu=menu,boss=player)
     for i in Staff_list:
-        if i.busy==False:
+        if i.busy==False and i.id not in freeStaff:
            freeStaff.append(i.id)
-    print("_"*100)
+    print("_"*220)
     a = int(input('Введите нужную функцию: '))
-    print("_"*100)
+    print("_"*220)
     if a == 1:
         if len(freeStaff)>=1:
             print('Свободные рабочие:')
             for i in freeStaff:
-                print(f'ID-индетификатор сотрудника: {i}\nИмя: {Staff_list[i-1].name}\nУровень: {Staff_list[i-1].level}\n{"_"*100}')
+                print(f'ID-индетификатор сотрудника: {i}\nИмя: {Staff_list[i-1].name}\nУровень: {Staff_list[i-1].level}\n{"_"*220}')
             inp=int(input('Введите ID-индетификатор нужного сотрудника: '))
             while inp not in freeStaff:
                     inp=int(input('Данный сотрудник занят/несуществует. Повторите ввод: '))
             print('Выберите стол:')
             for i in busyTable:
-                print(f'Номер стола: {i}\nЗаказ: {Tables_list[i-1].order}\n{"_"*100}')
+                print(f'Номер стола: {i}\nЗаказ: {Tables_list[i-1].order}\n{"_"*220}')
             inp2=int(input('Введите номер нужного стола: '))
             while inp2 not in busyTable:
                 inp2=int(input('Данный стол свободен/несуществует. Повторите ввод:'))
             Tables_list[inp2-1].free=True
-            busyTable.clear()
+            busyTable.pop(inp2-1)
             asyncio.run(Staff_list[inp-1].cook(Tables_list[inp2-1],player))
 
 
@@ -55,7 +53,7 @@ while True:
         player.bosslvlup()
     if a == 3:
         for i in Staff_list:
-            print(f'ID-индетификатор сотрудника: {i}\nИмя: {i.name}\nУровень: {i.level}\n{"_"*100}')
+            print(f'ID-индетификатор сотрудника: {i}\nИмя: {i.name}\nУровень: {i.level}\n{"_"*220}')
         inp=int(input('Выберите сотрудника которому повысить уровень: '))
         player.personlvlup(Staff_list[inp-1])
     if a==4:
